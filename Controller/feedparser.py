c17fe72c52c2ebfb9e1bd6714701694fae536aa4
Cuyler Eastman <60369884+CuylerE@users.noreply.github.com>
2020-05-04 16:36:44 -0600

    Added error handling for invalid URL's

--- Controller/feedparser.py
+++ Controller/feedparser.py
@@ -1,5 +1,6 @@
 from urllib.request import urlopen
 from urllib.error import HTTPError
+from urllib.error import URLError
 import random
 import time
 from bs4 import BeautifulSoup # pip install beautifulsoup4
@@ -18,7 +19,7 @@ def parse(url):
             return build_library(BeautifulSoup(page, "xml")) # pip install lxml
         except HTTPError:
             if(counter >= 3):
-                print("Something Went Wrong. Check Feeds.txt for invalid / Expired urls")
+                print("Something Went Wrong. The destination isn't accepting requests")
                 break
             print("Too many Requests, retrying.")
             time.sleep(3)
@@ -26,6 +27,9 @@ def parse(url):
             continue
         except ValueError:
             print("Invalid url found, consider correcting.")
+            return " "
+        except URLError:
+            print("Invalid url found, cannot load RSS / Atom")
             return " "
         else:
             break
